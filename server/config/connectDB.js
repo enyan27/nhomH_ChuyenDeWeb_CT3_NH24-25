e099fd4a0af67e7330089a1310f03a1aab3f8db6
enyan27 <truongdat2325@gmail.com>
2025-03-25 13:35:25 +0700

    chore: fix connection string

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 mongoose
-  .connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@twitter.vz9ss.mongodb.net/twitter-db`, {
-  // .connect("mongodb://localhost:27017/twitter-db", {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
+  .connect(`${process.env.DB_URI}`, {
+    // .connect("mongodb://localhost:27017/twitter-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
   .then(() => {
     console.log('\x1b[32m%s\x1b[0m', "Database connected");
